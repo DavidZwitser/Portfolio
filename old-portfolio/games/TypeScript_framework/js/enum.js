34e13f269defea72e6bb120961dfe631ec79f8a1
DavidZwitser <davidzwitser.com>
2019-06-18 18:01:00 +0200

    Made 3.0 branch ready for development

--- old-portfolio/games/TypeScript_framework/js/enum.js
+++
@@ -1,7 +0,0 @@
-var ObjectTypes;
-(function (ObjectTypes) {
-    ObjectTypes[ObjectTypes["Empty"] = 0] = "Empty";
-    ObjectTypes[ObjectTypes["Light"] = 1] = "Light";
-    ObjectTypes[ObjectTypes["Shape"] = 2] = "Shape";
-})(ObjectTypes || (ObjectTypes = {}));
-//# sourceMappingURL=enum.js.map
--- projects/3DRenderer/Src/Extra's/Clone.js
+++
@@ -1,8 +0,0 @@
-function clone(obj) {
-    var returnArray = [];
-    for (var o = 0; o < obj.length; o++) {
-        var currObj = obj[o];
-        returnArray[o] = new Vector3(currObj.x, currObj.y, currObj.z);
-    }
-    return returnArray;
-}
