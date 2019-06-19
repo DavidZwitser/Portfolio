f083d51764f323ae9e1886a4c9e25d6d8176a1c3
DavidZwitser <davidzwitser.com>
2019-06-19 14:43:05 +0200

    Added navigation bar

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,13 +2,31 @@
 import '../css/index.scss';
 
 /* Import animation library */
-import anime from '../../node_modules/animejs/lib/anime.es.js';
-/// <reference path="../definitions/index.d.ts" />
+// import anime from '../../node_modules/animejs/lib/anime.es.js';
+// /// <reference path="../definitions/index.d.ts" />
 
-anime({
-    targets: '.css-selector-demo',
-    translateX: 250,
-    translateY: 300,
-    delay: 1000,
-    easing: 'easeInOutExpo'
-});
+window.onhashchange = hashChanged;
+window.onload = hashChanged;
+
+function hashChanged()
+{
+    let hash = window.location.hash;
+
+    let navbar = document.getElementById("navigation-bar");
+    let navbar_links = document.getElementById('navigation-bar-links');
+    let navbar_back = document.getElementById('navigation-bar-back-button');
+    
+    if (hash == '#page-home')
+    {
+        navbar.style.top = "94vh";
+        navbar_links.style.display = 'block';
+        navbar_back.style.display = 'none';
+    }
+    else
+    {
+        navbar.style.top = '0%';
+        navbar_links.style.display = 'none';
+        navbar_back.style.display = 'block';
+    }
+}
+
